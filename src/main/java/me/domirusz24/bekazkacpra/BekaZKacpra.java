b9730c9652674b23a2f9731a0b9e389a42417f2a
domirusz24 <DomiRusz24@gmail.com>
2023-06-08 22:41:22 +0200

    jebać kacpra

--- src/main/java/me/domirusz24/bekazkacpra/BekaZKacpra.java
+++ src/main/java/me/domirusz24/bekazkacpra/BekaZKacpra.java
@@ -1,17 +1,51 @@
 package me.domirusz24.bekazkacpra;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BekaZKacpra extends JavaPlugin {
+public final class BekaZKacpra extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        HandlerList.unregisterAll((Listener) this);
+    }
+
+    @EventHandler
+    public void onMessage(AsyncPlayerChatEvent event) {
+        if (event.getPlayer().getName().equals("kacpeer35")) {
+            event.setMessage(event.getMessage() + " UwU");
+        }
+    }
+
+    @EventHandler
+    public void onHit(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player) {
+            Player attacker = (Player) event.getDamager();
+            if (attacker.getName().equals("kacpeer35")) {
+                event.setDamage(event.getDamage() / 2);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player entity = (Player) event.getEntity();
+            if (entity.getName().equals("kacpeer35")) {
+                event.setDamage(event.getDamage() * 2);
+            }
+        }
     }
 }
